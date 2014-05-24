c6688bec344d9dc75edac1286d09fe83f4c751ad
nokia6630 <nokia6630@tlen.pl>
2014-05-24 14:59:16 +0200

    example insert to MongoDB

--- src/main/java/com/pik/moviecollection/server/HelloController.java
+++ src/main/java/com/pik/moviecollection/server/HelloController.java
@@ -1,7 +1,12 @@
 package com.pik.moviecollection.server;
 
+import com.pik.moviecollection.model.Movie;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 
 @RestController
@@ -14,5 +19,18 @@ public class HelloController {
         String result="123Witojcie panocki na moim websajcie! I nie piszta mnie tu: "+name;
         return result;
         //return new ResponseEntity<String>("{value: " + result + "}", HttpStatus.OK);
+    }
+
+    private void exampleInsertToMongoDB()
+    {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("kunderapu");
+	EntityManager em = emf.createEntityManager();
+
+	Movie movie = new Movie();
+	movie.movieID = "1";
+	movie.country = "PL";
+	movie.title = "PIK";
+
+	em.persist(movie);
     }
 }
