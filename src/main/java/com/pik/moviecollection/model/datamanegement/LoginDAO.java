0f9af3b2b01ee5219c11aca53f05e58de6c9d8b5
Piotr Siczek <spiczek@gmail.com>
2014-05-26 01:46:56 +0200

    login user scratch

---
+++ src/main/java/com/pik/moviecollection/model/datamanegement/LoginDAO.java
@@ -0,0 +1,39 @@
+package com.pik.moviecollection.model.datamanegement;
+
+import com.pik.moviecollection.model.orm.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ * Created by piotr on 26.05.14.
+ */
+public class LoginDAO {
+    private static EntityConnection conn = EntityConnection.getInstance();
+    private static LoginDAO ourInstance = new LoginDAO();
+    public static LoginDAO getInstance() {
+        return ourInstance;
+    }
+    private LoginDAO() {}
+
+
+    public static User loginUser(String login, String password) {
+
+        EntityManager em = conn.getConnection();
+        String queryString = "SELECT u FROM User u " +
+                "WHERE u.login = :login and u.pass = :pass";
+        Query query = em.createQuery(queryString);
+        query.setParameter("login", login);
+        query.setParameter("pass", password);
+
+        User result = (User)query.getResultList().get(0);
+
+        conn.closeConnection();
+        return result;
+    }
+
+
+
+
+
+}
