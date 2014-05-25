4cffe683a9b96b91bbe8f74af9ac46f5fe3c44f3
Piotr Siczek <spiczek@gmail.com>
2014-05-26 01:39:03 +0200

    User entity

---
+++ src/main/java/com/pik/moviecollection/model/orm/User.java
@@ -0,0 +1,63 @@
+package com.pik.moviecollection.model.orm;
+
+import javax.persistence.*;
+
+/**
+ * Created by piotr on 26.05.14.
+ */
+@Entity
+@Table(name = "USER", schema = "MovieCollection@kunderapu")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name="USER_ID")
+    private String userID;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "LOGIN")
+    private String login;
+
+    @Column(name = "PASSWORD")
+    private String pass;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
