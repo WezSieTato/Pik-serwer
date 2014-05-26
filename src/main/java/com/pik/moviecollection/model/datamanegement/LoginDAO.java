6a043baaab2230c3491eabcc02a88cebddf6ee1a
Piotr Siczek <spiczek@gmail.com>
2014-05-26 12:57:19 +0200

    login user return value LoginResult

--- src/main/java/com/pik/moviecollection/model/datamanegement/LoginDAO.java
+++ src/main/java/com/pik/moviecollection/model/datamanegement/LoginDAO.java
@@ -2,6 +2,7 @@ package com.pik.moviecollection.model.datamanegement;
 
 import com.pik.moviecollection.model.orm.Token;
 import com.pik.moviecollection.model.orm.User;
+import com.pik.moviecollection.model.result.data.LoginResult;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -21,7 +22,7 @@ public class LoginDAO {
      * @return null dla blednych danych
      *         User dane zalogowanego uzytkownika
      */
-    public static User loginUser(String login, String password) {
+    public static LoginResult loginUser(String login, String password) {
 
         EntityManager em = conn.getConnection();
         String queryString = "SELECT u FROM User u WHERE u.login = :login and u.pass = :pass";
@@ -36,7 +37,7 @@ public class LoginDAO {
         em.persist(token);
 
         conn.closeConnection();
-        return result;
+        return new LoginResult(result, token);
     }
 
 
