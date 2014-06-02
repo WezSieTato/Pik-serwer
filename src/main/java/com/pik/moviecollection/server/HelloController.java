ecfecd854bbd61adfebcd903d960e2c9765dbc9a
Losiowaty <elch1989@gmail.com>
2014-06-02 18:06:06 +0200

    PIK-20 Naprawa serwera branch push

--- src/main/java/com/pik/moviecollection/server/HelloController.java
+++ src/main/java/com/pik/moviecollection/server/HelloController.java
@@ -25,6 +25,10 @@ public class HelloController {
         //return new ResponseEntity<String>("{value: " + result + "}", HttpStatus.OK);
     }
 
+    private void dummy() {
+        
+    }
+
     private void addTestData()
     {
 	EntityManager connection = EntityConnection.getConnection();
