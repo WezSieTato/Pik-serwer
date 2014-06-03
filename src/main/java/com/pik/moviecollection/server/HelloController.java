01830bbc5a35d5aa8767b87c673d28be2ec649d5
nokia6630 <nokia6630@tlen.pl>
2014-06-03 23:29:23 +0200

    PIK-20 Naprawa serwera proba 10

--- src/main/java/com/pik/moviecollection/server/HelloController.java
+++ src/main/java/com/pik/moviecollection/server/HelloController.java
@@ -18,8 +18,8 @@ public class HelloController {
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public String getGreeting(@PathVariable String name) {
         String result="Witojcie panocki na moim websajcie! I nie piszta mnie tu: "+name;
-	addTestData();
-	//result = getMovies(result);
+	//addTestData();
+	result = getMovies(result);
 
 	return result;
         //return new ResponseEntity<String>("{value: " + result + "}", HttpStatus.OK);
